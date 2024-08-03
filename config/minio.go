afe02ef6edaea690f4e080610f1f394081c18db1
elskow <elskow@outlook.com>
2024-08-04 03:53:53 +0700

    feat: centeralized .env based minio config

--- config/minio.go
+++ config/minio.go
@@ -2,13 +2,23 @@ package config
 
 import (
 	"context"
+	"github.com/joho/godotenv"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
+	"os"
 )
 
 func InitMinio() (*minio.Client, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, err
+	}
+
+	accessKey := os.Getenv("MINIO_ACCESS_KEY")
+	secretKey := os.Getenv("MINIO_SECRET_KEY")
+
 	client, err := minio.New("localhost:9000", &minio.Options{
-		Creds:  credentials.NewStaticV4("minio", "minio123", ""),
+		Creds:  credentials.NewStaticV4(accessKey, secretKey, ""),
 		Secure: false,
 	})
 	if err != nil {
